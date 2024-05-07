cb69541916a62ac7ef85abcfa1dce62e9e09dd7d
Manish <mdashsharma95@gmail.com>
2024-05-08 01:12:00 +0530

    Dockerize The Application 🛳️

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -5,7 +5,7 @@ dotenv.config()
 const config = {
     PORT : process.env.PORT || 9000,
     IP: process.env.IP,
-    MONGO_DB_URI : process.env.MONGO_DB_URI,
+    MONGO_DB_URI : process.env.MONGO_DB_URI || "mongodb://mongo:27017/trackyourpayment",
     JWT_EXPIRY : process.env.JWT_EXPIRY,
     JWT_SECRET : process.env.JWT_SECRET,
     REFRESH_TOKEN_SECRET : process.env.REFRESH_TOKEN_SECRET,
